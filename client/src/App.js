47e42a658ccb562e7756bb26463766da16ec4a60
VrajLalwala22 <vrajlalwalawork@gmail.com>
2025-01-19 18:08:59 +0530

    home page

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import BookingCar from './pages/BookingCar'
+
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Router> {/* BrowserRouter is correctly imported as Router */}
+        <Routes> {/* Wrap all Route components inside Routes */}
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/bookingcar" element={<BookingCar />} />
+        </Routes>
+      </Router>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/pages/BookingCar.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+function BookingCar()
+{
+    return(
+        <div>
+            <h1>BookingCar pages</h1>
+        </div>
+    )
+}
+
+
+export default BookingCar
